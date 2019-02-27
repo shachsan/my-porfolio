899cb997fc8216d446c0b147cd34b40b6effa098
Sanjay Sai <shachsan@hotmail.com>
2019-02-27 12:29:07 -0500

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainContainerComponent } from './main-container/main-container.component';
+import { CenterContainerComponent } from './center-container/center-container.component';
+import { RightContainerComponent } from './right-container/right-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainContainerComponent,
+    CenterContainerComponent,
+    RightContainerComponent,
   ],
   imports: [
     BrowserModule,
