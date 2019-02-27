64c35babb95a7521ed770c1afd4d2b486232a036
Sanjay Sai <shachsan@hotmail.com>
2019-02-27 11:41:26 -0500

    create app skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainContainerComponent } from './main-container/main-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainContainerComponent,
   ],
   imports: [
     BrowserModule,
