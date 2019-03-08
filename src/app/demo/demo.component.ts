cf0c4e89a349407cfb08a7fb5f74b87247fdc04c
Sanjay Sai <shachsan@hotmail.com>
2019-03-07 23:51:10 -0500

    wrap up for the day

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -18,29 +18,14 @@ export class DemoComponent implements OnInit {
     'easy-source':'https://www.youtube.com/embed/x1uVlIx_1HQ?rel=0&modestbranding=1&autohide=1&showinfo=0&autoplay=1'
   }
 
-  constructor(private sanitizer: DomSanitizer) {
-    // javascript: URLs are dangerous if attacker controlled.
-    // Angular sanitizes them in data binding, but you can
-    // explicitly tell Angular to trust this value:
-    // this.updateVideoUrl('PUBnlbjZFAI');
-  }
+  constructor(private sanitizer: DomSanitizer) {}
   
-  updateVideoUrl(id: string) {
-    // Appending an ID to a YouTube URL is safe.
-    // Always make sure to construct SafeValue objects as
-    // close as possible to the input data so
-    // that it's easier to check if the value is safe.
-    console.log('demoToPlay', this.demoToPlay);
-    console.log('demo url', this.demoUrl[this.demoToPlay]);
-    console.log('safe url', this.videoUrl);
-  }
   
   ngOnInit() {
-    console.log('demo compo', this.demoToPlay);
   }
   
   getUrlForDemo(){
-    this.dangerousVideoUrl = this.demoUrl[this.demoToPlay] + 'PUBnlbjZFAI';
+    this.dangerousVideoUrl = this.demoUrl[this.demoToPlay] + '&PUBnlbjZFAI';
     this.videoUrl =
     this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousVideoUrl);
     return this.videoUrl;
