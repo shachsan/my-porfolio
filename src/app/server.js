efa65feedfc79bdde13728fbb288cfd931a93f70
Sanjay Sai <shachsan@hotmail.com>
2019-03-09 23:34:22 -0500

    remove __dirname from server page

--- src/app/server.js
+++ src/app/server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/sanjay-sai'));
+app.use(express.static('/dist/sanjay-sai'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/sanjay-sai/index.html'));
+res.sendFile(path.join('/dist/sanjay-sai/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
