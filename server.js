f72109e2bc87a026c0417f3ed06a507fed4b8c2a
Sanjay Sai <shachsan@hotmail.com>
2019-03-09 23:42:40 -0500

    change app name to project name in server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/sanjay-sai'));
+app.use(express.static('/dist/porfolio'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('/dist/sanjay-sai/index.html'));
+res.sendFile(path.join('/dist/portfolio/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
