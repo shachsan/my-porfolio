14e2f96b640b9912dd344c41be1a4007869b9552
Sanjay Sai <shachsan@hotmail.com>
2019-03-09 23:58:46 -0500

    move index file to project root directory

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static('/dist/portfolio'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname + '/dist/index.html'));
+res.sendFile(path.join('/dist/portfolio/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
