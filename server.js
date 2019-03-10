05f46a01434f1b7c68a55ffad2b25c72ab381b85
Sanjay Sai <shachsan@hotmail.com>
2019-03-10 13:31:17 -0400

    add app name

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/portfolio'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname + '/dist/index.html'));
+res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
