b3324d34690250bab5007439d4e9ae489e711ae2
Sanjay Sai <shachsan@hotmail.com>
2019-03-10 00:24:06 -0500

    try again

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./src'));
+app.use(express.static('./dist/portfolio'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('./index.html'));
+res.sendFile(path.join('./dist/portfolio/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
